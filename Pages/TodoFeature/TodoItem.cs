e7b11c2e843c293006c481966713cc5d232e3776
kbetkowsky <92058076+kbetkowsky@users.noreply.github.com>
2022-09-25 20:35:10 +0200

    Add files via upload

---
+++ Pages/TodoFeature/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectOne.Pages.TodoFeature
+{
+    public class TodoItem
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
---
+++ Pages/TodoFeature/TodoState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectOne.Pages.TodoFeature
+{
+    public class TodoState
+    {
+        public List<TodoItem> TodoItems { get; set; } = new();
+
+        public bool CanClear => !TodoItems.Any(i => i.IsDone);
+
+        public void AddTodo(TodoItem newTodo)
+        {
+            TodoItems.Add(newTodo);
+        }
+        public void ClearTodos()
+        {
+            TodoItems = TodoItems.Where(i => !i.IsDone).ToList();
+        }
+    }
+}
